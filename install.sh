6d2fb2d565d3a85249f2d0504e6e476f6371bfcc
GabrielLud <57450444+GabrielLud@users.noreply.github.com>
2021-03-11 20:00:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 pkg upgrade
 cd
-git clone https://github.com/GabrielLud/BOT
+git clone https://github.com/GabrielLud/BOT-
 clear
-cd ./BOT
+cd ./BOT-
 bash install.sh
 cd
-cd ./BOT
+cd ./BOT-
 npm start
 
 echo "[*] obrigado por utilizar o bot \"darkbot\" dark agradece"
